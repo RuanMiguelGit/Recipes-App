e59b3ad3f8ac96b5321ae4b7f0722c7dd9b72245
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-03-19 22:33:47 -0300

    REQ 05 - Validating login form

--- src/components/InputText.js
+++ src/components/InputText.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
 
-const InputText = ({ name, label, updateState, value }) => (
+const InputText = ({ name, label, updateState, value, type }) => (
   <TextField
     variant="outlined"
     id={ name }
     label={ label }
+    type={ type }
     value={ value }
     onChange={ (evt) => updateState(evt.target.value) }
     inputProps={ { 'data-testid': name } }
@@ -19,6 +20,7 @@ InputText.propTypes = {
   label: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   updateState: PropTypes.func.isRequired,
+  type: PropTypes.string.isRequired,
 };
 
 export default InputText;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
@@ -8,6 +8,27 @@ import './Login.css';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [disableSubmit, setDisableSubmit] = useState(true);
+
+  // Following RegEx found at: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+  // I had to split the RegEx to fix the linter issues. I've found how to solve it here:
+  // https://stackoverflow.com/questions/12317049/how-to-split-a-long-regular-expression-into-multiple-lines-in-javascript
+  const validEmail = (userEmail) => {
+    const emailRegex = new RegExp([
+      '^(([^<>()[\\]\\.,;:\\s@\\"]+(\\.[^<>()',
+      '[\\]\\.,;:\\s@\\"]+)*)|(\\".+\\"))@(([^<>()',
+      '[\\]\\.,;:\\s@\\"]+\\.)+[^<>()[\\]\\.,;:\\s@\\"]{2,})$',
+    ].join(''));
+    return emailRegex.test(userEmail);
+  };
+  useEffect(() => {
+    const MINIMAL_PASSWORD_LENGTH = 6;
+    if (password.length > MINIMAL_PASSWORD_LENGTH && validEmail(email)) {
+      setDisableSubmit(false);
+    } else {
+      setDisableSubmit(true);
+    }
+  }, [password, email]);
 
   return (
     <Grid
@@ -21,6 +42,7 @@ const Login = () => {
           <InputText
             label="Email"
             name="email-input"
+            type="email"
             value={ email }
             updateState={ setEmail }
           />
@@ -31,7 +53,12 @@ const Login = () => {
             value={ password }
             updateState={ setPassword }
           />
-          <Button type="submit" variant="contained" data-testid="login-submit-btn">
+          <Button
+            type="submit"
+            variant="contained"
+            data-testid="login-submit-btn"
+            disabled={ disableSubmit }
+          >
             Login
           </Button>
         </Card>
